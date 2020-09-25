227d1dd343d9c12ace22f87856b88d88146da28a
Mia <miadong179@gmail.com>
2020-09-25 20:40:24 +0200

    DiceGame1_medGUI

--- src/main/java/Play/DiceGame.java
+++ src/main/java/Play/DiceGame.java
@@ -1,5 +1,7 @@
 package Play;
-
+import gui_fields.GUI_Field;
+import gui_main.GUI;
+import java.awt.*;
 import java.util.Scanner;
 
 public class DiceGame {
@@ -8,9 +10,15 @@ public class DiceGame {
         Dice die1 = new Dice();
         Dice die2 = new Dice();
 
+        //GUI
+        GUI.setNull_fields_allowed(true);
+        GUI_Field[] fields = new GUI_Field[40];
+
+        GUI gui = new GUI(fields,Color.PINK);
+
         Scanner rafle = new Scanner(System.in);
         System.out.println("SHAKE RAFFLE CUP \nby typing >>roll<<");
-//comment
+
         while (true) {
             String s = rafle.nextLine();
             if (s.equalsIgnoreCase("roll")) {
@@ -19,13 +27,14 @@ public class DiceGame {
 
                 System.out.println(die1.getFaceValue());
                 System.out.println(die2.getFaceValue());
+                gui.setDice(die1.getFaceValue(), die2.getFaceValue());
             } else {
                 System.out.println(":( plz");
             }
 
-            if (die1.getFaceValue() == die2.getFaceValue()) {
-                System.out.println("Point");
-            }
+            //if (die1.getFaceValue() == die2.getFaceValue()) {
+              //  System.out.println("Point");
+            //}
 
         }
     }
